f0cb415bc7aceb32df53e28725fe328f6f364ee2
sandeepj161 <j.sandeep.private@gmail.com>
2025-08-04 10:37:30 +0530

    Update monitor.h

--- monitor.h
+++ monitor.h
@@ -1,10 +1,7 @@
 #pragma once
 
 int vitalsOk(float temperature, float pulseRate, float spo2);
-
 bool isTemperatureOk(float temperature);
 bool isPulseRateOk(float pulseRate);
 bool isSpO2Ok(float spo2);
-
-
 
