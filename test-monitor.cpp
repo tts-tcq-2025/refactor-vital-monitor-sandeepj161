1592f4d4496de1f9dfa01a6c2316da393105439d
sandeepj161 <j.sandeep.private@gmail.com>
2025-08-04 10:06:01 +0530

    Update test-monitor.cpp

--- test-monitor.cpp
+++ test-monitor.cpp
@@ -1,7 +1,36 @@
 #include <gtest/gtest.h>
 #include "./monitor.h"
 
+extern const float TEMPERATURE_MIN;
+extern const float TEMPERATURE_MAX;
+extern const float PULSE_MIN;
+extern const float PULSE_MAX;
+extern const float SPO2_MIN;
+
 TEST(Monitor, NotOkWhenAnyVitalIsOffRange) {
-  ASSERT_FALSE(vitalsOk(99, 102, 70));
-  ASSERT_TRUE(vitalsOk(98.1, 70, 98));
+  EXPECT_FALSE(vitalsOk(99, 102, 70));
+  EXPECT_TRUE(vitalsOk(98.1, 70, 98));
 }
+
+TEST(Monitor, TemperatureBoundary) {
+  EXPECT_TRUE(isTemperatureOk(95.0f));
+  EXPECT_TRUE(isTemperatureOk(102.0f));
+  EXPECT_FALSE(isTemperatureOk(94.9f));
+  EXPECT_FALSE(isTemperatureOk(102.1f));
+}
+
+TEST(Monitor, PulseBoundary) {
+  EXPECT_TRUE(isPulseRateOk(60.0f));
+  EXPECT_TRUE(isPulseRateOk(100.0f));
+  EXPECT_FALSE(isPulseRateOk(59.9f));
+  EXPECT_FALSE(isPulseRateOk(100.1f));
+}
+
+TEST(Monitor, SpO2Boundary) {
+  EXPECT_TRUE(isSpO2Ok(90.0f));
+  EXPECT_TRUE(isSpO2Ok(92.0f));
+  EXPECT_FALSE(isSpO2Ok(89.9f));
+  EXPECT_FALSE(isSpO2Ok(70.0f));
+}
+
+
